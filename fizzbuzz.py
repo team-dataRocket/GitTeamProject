c16b3cc1ce947f3b6c0d2b51c3c4457542676eae
ardor924 <ardor924@gmail.com>
2023-09-18 18:00:16 +0900

    feat : print buzz
    detail : add buzz sentence if i is multiple of 5

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,3 +2,7 @@ def fizz(num:int) :
     for i in range(1,num+1) :
         if i % 3 == 0 :
             print('fizz')
+        elif i % 5 == 0 :
+            print('buzz')
+        else :
+            print(i)
