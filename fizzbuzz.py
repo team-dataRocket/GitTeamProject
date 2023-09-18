058da1e8655f4cb9fed57974ec5a8298bb4c0309
ardor924 <ardor924@gmail.com>
2023-09-18 18:36:54 +0900

    add param +111

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,5 +1,5 @@
 def fizzbuzz(num:int) :
-    for i in range(1, num+1):
+    for i in range(1, num+111):
         if i%3==0 or i%5==0:
             print('fizz'*(i%3==0)+'buzz'*(i%5==0))
         else :
