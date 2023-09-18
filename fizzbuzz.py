6c31d0fbd6f661cc851382e2aa9ce63378bdec96
ardor924 <ardor924@gmail.com>
2023-09-18 18:02:42 +0900

    feat : print fizzbuzz
    detail : if multiple of 15 is inputed print fizzbuzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,8 +1,11 @@
 def fizz(num:int) :
     for i in range(1,num+1) :
-        if i % 3 == 0 :
-            print('fizz')
-        elif i % 5 == 0 :
-            print('buzz')
-        else :
-            print(i)
+        if i == 15 :
+            print('fizzbuzz')
+        else :     
+            if i % 3 == 0 :
+                print('fizz')
+            elif i % 5 == 0 :
+                print('buzz')
+            else :
+                print(i)
